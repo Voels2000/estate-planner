733392186bbff5effd3962f0b28655feffd1506e
Alan Voels <avoels@comcast.net>
2026-03-25 13:21:23 -0700

    fix: correct feedback-button import path

--- app/(dashboard)/_dashboard-client.tsx
+++ app/(dashboard)/_dashboard-client.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Link from 'next/link'
-import { FeedbackButton } from '@/components/feedback-button'
+import { FeedbackButton } from './components/feedback-button'
 
 type SetupStep = {
   key: string
